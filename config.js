6f5cb58a33ae649909038646607bf3ef7f02fe50
Codey McDaniel <codeymcdaniel@gmail.com>
2020-11-04 06:44:18 -0700

    removed db link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI || 'mongodb+srv://codeycsm:GDZfinnUDGy6wzaf@cluster0.b2ixv.mongodb.net/book-list?retryWrites=true&w=majority',
+    mongoUri: process.env.MONGO_URI,
     PORT: process.env.PORT || 5000
 }
