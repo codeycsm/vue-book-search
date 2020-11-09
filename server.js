6980c32ca757b44d145d5fdd938cc95357a7ccbe
Codey McDaniel <codeymcdaniel@gmail.com>
2020-11-09 07:11:39 -0700

    db connection updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
